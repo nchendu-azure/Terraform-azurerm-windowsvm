deed3e178efeeb968136a2b94af4288faa4b0498
N <cnagaraj@in.imshealth.com>
2023-03-05 00:12:28 +0530

    edited variable file

---
+++ variable.tf
@@ -0,0 +1,32 @@
+variable "resoure_group_name" {
+  description = "Resource Group Name"
+  type        = string
+  default     = "ODL-azure-858691"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "vmname" {
+  type = string
+}
+
+variable "size" {
+  type = string
+
+}
+variable "uname" {
+  type = string
+}
+
+variable "pword" {
+  type = string
+
+}
+
+
+
+
+  
